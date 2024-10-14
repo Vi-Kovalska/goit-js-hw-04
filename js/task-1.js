85e4bd201aa1ad16d7dae6aa6dafae04f0efbe57
Viktoriia-Kovalska <kovalskayavi853@gmail.com>
2024-10-14 22:03:55 +0300

    refactoring of code in the first task

--- js/task-1.js
+++ js/task-1.js
@@ -5,7 +5,7 @@ function isEnoughCapacity(products, containerSize) {
     for (const item of arreyQuantityOfProducts) {
         totalQuantity += item;
     }
-    return totalQuantity <= containerSize ? true : false;
+    return totalQuantity <= containerSize;
 }
 
   console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
